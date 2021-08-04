adb831f2f25fb1ac163f9a2ced6c32c6fbec7755
tybruno <tybruno@cisco.com>
2021-08-04 09:27:22 -0700

    refactoring

--- assertify/logic.py
+++ assertify/logic.py
@@ -1,5 +1,4 @@
 from dataclasses import dataclass, field
-from typing import Optional, Union, Type
 import unittest_assertions.logic
 from assertify.base import BuiltinAssertionAssertify
 
---
+++ assertify/regex.py
@@ -0,0 +1,51 @@
+from dataclasses import dataclass, field
+import unittest_assertions.regex
+from assertify.base import BuiltinAssertionAssertify
+
+
+@dataclass
+class AssertifyRaisesRegex(BuiltinAssertionAssertify):
+    assertion_cls: unittest_assertions.regex.AssertRaisesRegex = field(
+        default=unittest_assertions.regex.AssertRaisesRegex, init=False
+    )
+
+    def __call__(self, expected_exception, expected_regex, **kwargs):
+        super().__call__(
+            expected_exception=expected_exception,
+            expected_regex=expected_regex,
+            **kwargs
+        )
+
+
+@dataclass
+class AssertifyWarnsRegex(BuiltinAssertionAssertify):
+    assertion_cls: unittest_assertions.regex.AssertWarnsRegex = field(
+        default=unittest_assertions.regex.AssertWarnsRegex, init=False
+    )
+
+    def __call__(self, expected_warning, expected_regex, **kwargs):
+        super().__call__(
+            expected_warning=expected_warning,
+            expected_regex=expected_regex,
+            **kwargs
+        )
+
+
+@dataclass
+class AssertifyRegex(BuiltinAssertionAssertify):
+    assertion_cls: unittest_assertions.regex.AssertRegex = field(
+        default=unittest_assertions.regex.AssertRegex, init=False
+    )
+
+    def __call__(self, text, expected_regex):
+        super().__call__(text=text, expected_regex=expected_regex)
+
+
+@dataclass
+class AssertifyNotRegex(BuiltinAssertionAssertify):
+    assertion_cls: unittest_assertions.regex.AssertNotRegex = field(
+        default=unittest_assertions.regex.AssertNotRegex, init=False
+    )
+
+    def __call__(self, text, unexpected_regex):
+        super().__call__(text=text, unexpected_regex=unexpected_regex)
