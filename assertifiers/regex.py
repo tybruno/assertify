baed0654d2af573151a6f2f494a2fab4aed8fcfc
tybruno <tybruno@cisco.com>
2021-12-01 15:42:50 -0700

    added docstrings for regex.py

--- assertifiers/regex.py
+++ assertifiers/regex.py
@@ -1,8 +1,9 @@
+import re
 from dataclasses import (
     dataclass,
     field,
 )
-
+from typing import Type, Union, Tuple
 import unittest_assertions.regex
 
 from assertifiers.base import BuiltinAssertionAssertify
@@ -10,53 +11,160 @@ from assertifiers.base import BuiltinAssertionAssertify
 
 @dataclass
 class AssertifyRaisesRegex(BuiltinAssertionAssertify):
+    """assertify that the message in a raised exception matches a regex
+
+    Example:
+        >>> assert_raises_regex = AssertifyRaisesRegex(raises=None)
+        >>> assert_raises_regex(ValueError, "invalid literal for.*XYZ'$",
+        ... int, 'XYZ')
+        True
+    """
+
     _assertion_cls: unittest_assertions.regex.AssertRaisesRegex = field(
         default=unittest_assertions.regex.AssertRaisesRegex, init=False
     )
 
     def __call__(
-        self, expected_exception, expected_regex, *args, **kwargs
+        self,
+        expected_exception: Union[
+            Type[BaseException], Tuple[Type[BaseException]]
+        ],
+        expected_regex: Union[re.Pattern, str],
+        *args,
+        **kwargs
     ) -> bool:
-        return super().__call__(
+        """assertify
+
+        Args:
+            expected_exception: expected exception to be raised
+            expected_regex: expected regex during `expected_exception` is raised
+            *arg: Function to be called and extra positional args
+            **kwargs: Extra kwargs.
+
+        Returns:
+            `True` if the message in a raised exception matches a regex
+
+        """
+        result: bool = super().__call__(
             expected_exception,
             expected_regex,
             *args,
             **kwargs,
         )
+        return result
 
 
 @dataclass
 class AssertifyWarnsRegex(BuiltinAssertionAssertify):
+    """assertify that the message in a triggered warning matches a regexp.
+
+    Example:
+        >>> import warnings
+        >>>
+        >>> def legacy_function(msg):
+        ...     warnings.warn(msg,DeprecationWarning)
+        >>> assert_warns_regex = AssertifyWarnsRegex(raises=None)
+        >>> assert_warns_regex(DeprecationWarning, r'deprecated',
+        ... legacy_function,r'legacy_function is deprecated')
+        True
+    """
+
     _assertion_cls: unittest_assertions.regex.AssertWarnsRegex = field(
         default=unittest_assertions.regex.AssertWarnsRegex, init=False
     )
 
     def __call__(
-        self, expected_warning, expected_regex, *args, **kwargs
+        self,
+        expected_warning: Type[Warning],
+        expected_regex: Union[re.Pattern, str],
+        *args,
+        **kwargs
     ) -> bool:
-        return super().__call__(
+        """
+
+        Args:
+            expected_warning: `Warning~ class expected to be raised
+            expected_regex: Regex expected to be found in error message
+            *args: Function to be called and extra postional args
+            **kwargs: Extra kwargs
+
+        Returns:
+            `True` when the regex of the message in the triggered `Warning`
+        """
+        result: bool = super().__call__(
             expected_warning,
             expected_regex,
             *args,
             **kwargs,
         )
+        return result
 
 
 @dataclass
 class AssertifyRegex(BuiltinAssertionAssertify):
+    """assertify `text` matches `expected_regex`
+
+    Example:
+        >>> text = "Ala ma kota"
+        >>> assert_regex = AssertifyRegex(raises=None)
+        >>> assert_regex(text, r"k.t")
+        True
+        >>> assert_regex(text, r"wrong")
+        False
+    """
+
     _assertion_cls: unittest_assertions.regex.AssertRegex = field(
         default=unittest_assertions.regex.AssertRegex, init=False
     )
 
-    def __call__(self, text, expected_regex) -> bool:
-        return super().__call__(text=text, expected_regex=expected_regex)
+    def __call__(
+        self, text: str, expected_regex: Union[re.Pattern, str]
+    ) -> bool:
+        """assertify `text` matches `expected_regex`
+
+        Args:
+            text: checked to see if will match `expected_regex`
+            expected_regex: checked to see if it matched `text`
+
+        Returns:
+            `True` if `text` matches `expected_regex`
+        """
+        result: bool = super().__call__(
+            text=text, expected_regex=expected_regex
+        )
+        return result
 
 
 @dataclass
 class AssertifyNotRegex(BuiltinAssertionAssertify):
+    """assertify `text` does not match `unexpected_regex`
+
+    Example:
+        >>> text = "Ala ma kota"
+        >>> assert_regex = AssertifyNotRegex(raises=None)
+        >>> assert_regex("Ala ma kota", r"wrong")
+        True
+        >>> assert_regex(text, r"k.t")
+        False
+    """
+
     _assertion_cls: unittest_assertions.regex.AssertNotRegex = field(
         default=unittest_assertions.regex.AssertNotRegex, init=False
     )
 
-    def __call__(self, text, unexpected_regex) -> bool:
-        return super().__call__(text=text, unexpected_regex=unexpected_regex)
+    def __call__(
+        self, text: str, unexpected_regex: Union[re.Pattern, str]
+    ) -> bool:
+        """assertify `text` does not match `unexpected_regex`
+
+        Args:
+            text: checked to see that it does not match `unexpected_regex`
+            unexpected_regex: checked to see that it does not match `text`
+
+        Returns:
+            `True` if `text` does not match `unexpected_regex`
+        """
+        result: bool = super().__call__(
+            text=text, unexpected_regex=unexpected_regex
+        )
+        return result
