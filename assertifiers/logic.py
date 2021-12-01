9bbdd70722bbece0100eb20e2484946730d7f8c1
tybruno <tybruno@cisco.com>
2021-12-01 14:59:30 -0700

    adding docstrings to logic.py

--- assertifiers/logic.py
+++ assertifiers/logic.py
@@ -2,7 +2,7 @@ from dataclasses import (
     dataclass,
     field,
 )
-
+from typing import Any
 import unittest_assertions.logic
 
 from assertifiers.base import BuiltinAssertionAssertify
@@ -10,12 +10,37 @@ from assertifiers.base import BuiltinAssertionAssertify
 
 @dataclass
 class LogicAssertify(BuiltinAssertionAssertify):
-    def __call__(self, expr) -> bool:
-        return super().__call__(expr=expr)
+    """Logic Assertify base class"""
+
+    def __call__(self, expr: Any) -> bool:
+        """assertify `expr`
+
+        Args:
+            expr: Expression that will be logically evaluated
+
+        Returns:
+            `True` if `expr` is logically True
+        """
+        result: bool = super().__call__(expr=expr)
+        return result
 
 
 @dataclass
 class AssertifyTrue(LogicAssertify):
+    """assertify `expr` is `True`
+
+    Example:
+        >>> assert_true = AssertifyTrue(raises=None)
+        >>> assert_true(True)
+        True
+        >>> assert_true(1)
+        True
+        >>> assert_true(0)
+        False
+        >>> assert_true(False)
+        False
+    """
+
     _assertion_cls: unittest_assertions.logic.AssertTrue = field(
         default=unittest_assertions.logic.AssertTrue, init=False
     )
@@ -23,6 +48,20 @@ class AssertifyTrue(LogicAssertify):
 
 @dataclass
 class AssertifyFalse(LogicAssertify):
+    """assert `expr` is False
+
+    Example:
+        >>> assert_false = AssertifyFalse(raises=None)
+        >>> assert_false(False)
+        True
+        >>> assert_false(0)
+        True
+        >>> assert_false(1)
+        False
+        >>> assert_false(True)
+        False
+    """
+
     _assertion_cls: unittest_assertions.logic.AssertFalse = field(
         default=unittest_assertions.logic.AssertFalse, init=False
     )
